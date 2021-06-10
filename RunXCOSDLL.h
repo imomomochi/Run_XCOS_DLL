556b4e98a509b8cdb84ab927e468de72f22594c6
imomochi <lion.toaster@gmail.com>
2021-06-10 23:06:31 +0900

    設定変更

--- RunXCOSDLL.h
+++ RunXCOSDLL.h
@@ -1,10 +1,14 @@
 
-#ifdef OUTDLL
-    #define DLL_API __declspec(dllexport)
+#ifndef NODLL
+    #ifdef OUTDLL
+        #define DLL_API __declspec(dllexport)
+    #else
+        #define DLL_API __declspec(dllimport) 
+    #endif
+    DLL_API int init(double *input_data,int input_size,double *output_data,int output_size);
+    DLL_API void operate(void);
 #else
-    #define DLL_API __declspec(dllimport) 
+    int init(double *input_data,int input_size,double *output_data,int output_size);
+    void operate(void);
 #endif
 
-
-DLL_API int init(double *input_data,int input_size,double *output_data,int output_size);
-DLL_API void operate(void);
